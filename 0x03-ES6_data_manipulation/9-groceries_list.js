932a07c2a2b5d4f8998943692fadd22ca580ab16
Eric Munyi <irerieric@gmail.com>
2023-07-04 21:38:46 +0000

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+function groceriesList() {
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+
+  return groceries;
+}
+
+export default groceriesList;
+
