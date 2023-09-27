caf8bb9c469c41f6ca567a61362bb06df2829ed4
Eric Munyi <irerieric@gmail.com>
2023-09-27 06:30:21 +0000

    typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,15 @@
-function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+/* eslint-disable */
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+
+  if (position > length - 1) {
+    throw new Error('Position outside range');
   }
 
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
+  data.setInt8(position, value);
 
-  return view;
-}
+  return data;
+};
 
 export default createInt8TypedArray;
-
