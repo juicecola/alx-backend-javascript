ba917b4eac88b20b37ae09d8e879c5ef3aa121db
Eric Munyi <irerieric@gmail.com>
2023-09-27 06:13:51 +0000

    linter

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,23 @@
-class Building {
+/* eslint-disable */
+
+export default class Building {
   constructor(sqft) {
-    if (new.target === Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    this.sqft = sqft;
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
 }
-
-export default Building;
-
